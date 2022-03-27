42906e9307b6482860f3d91843726a12e4d959f5
Anatoliy Fhockk <anatoliy.fhock@gmail.com>
2022-03-27 14:26:32 +0300

    templates changes

--- news/views.py
+++ news/views.py
@@ -6,20 +6,17 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.order_by('-created_at')
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'The List of news',
-        'categories': categories,
     }
     return render(request, 'news/index.html', context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
 
